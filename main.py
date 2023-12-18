2cce492fea5ea40377000d20731229c7f6e45b2e
Qomoxy <66842370+qomoxy@users.noreply.github.com>
2023-12-18 06:52:57 +0100

    nice

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Pile_File import Pile, File
 
 class Jeu:
     """Classe représentant un jeu de cartes."""
+
     def __init__(self, nb_joueurs: int = 2):
         """Création d'un jeu de cartes."""
         self.deck = Deck()
@@ -35,9 +36,76 @@ class Jeu:
         """Renvoie le paquet."""
         return self.__paquet
 
+    def gagnant(self):
+        """Renvoie le gagnant."""
+        if self.get_liste_joueurs()[0].est_vide():
+            return self.get_liste_joueurs()[1]
+        else:
+            return self.get_liste_joueurs()[0]
+
+    def bataille(self, Joueur1, Joueur2, carte1, carte2):
+        """Déroulement d'une bataille.
+        :param Joueur1: le joueur 1
+        :param Joueur2: le joueur 2
+        :param carte1: la carte du joueur 1
+        :param carte2: la carte du joueur 2
+        """
+        carte_cachee1 = Joueur1.get_paquet().defiler()
+        carte_cachee2 = Joueur2.get_paquet().defiler()
+        carte_dessus1 = Joueur1.get_paquet().defiler()
+        carte_dessus2 = Joueur2.get_paquet().defiler()
+
+        print("Bataille !")
+        print("Carte dessus du joueur 1 :", carte_dessus1)
+        print("Carte dessus du joueur 2 :", carte_dessus2)
+
+        if carte_dessus1.get_point() > carte_dessus2.get_point():
+
+            Joueur1.get_paquet().enfiler(carte1)
+            Joueur1.get_paquet().enfiler(carte2)
+            Joueur1.get_paquet().enfiler(carte_cachee1)
+            Joueur1.get_paquet().enfiler(carte_cachee2)
+
+        elif carte_dessus1.get_point() < carte_dessus2.get_point():
+
+            Joueur2.get_paquet().enfiler(carte1)
+            Joueur2.get_paquet().enfiler(carte2)
+            Joueur2.get_paquet().enfiler(carte_cachee1)
+            Joueur2.get_paquet().enfiler(carte_cachee2)
+
+        else:
+
+            self.bataille(Joueur1, Joueur2, carte1, carte2)
+
+    def tour(self, Joueur1, Joueur2):
+        """
+        Déroulement d'une partie.
+        :param Joueur1: le joueur 1
+        :param Joueur2: le joueur 2
+        """
+        while not Joueur1.est_vide() and not Joueur2.est_vide():
+
+            carte1 = Joueur1.get_paquet().defiler()
+            carte2 = Joueur2.get_paquet().defiler()
+            print("Tour", self.get_tour(), ":", carte1, "vs", carte2)
+
+            if carte1.get_point() > carte2.get_point():
+
+                Joueur1.get_paquet().enfiler(carte1)
+                Joueur1.get_paquet().enfiler(carte2)
+            elif carte1.get_point() < carte2.get_point():
+
+                Joueur2.get_paquet().enfiler(carte1)
+                Joueur2.get_paquet().enfiler(carte2)
+            else:
+                self.bataille(Joueur1, Joueur2, carte1, carte2)
+
     def partie(self):
         """Déroulement d'une partie."""
-        pass
+        while not self.get_liste_joueurs()[0].est_vide() and not self.get_liste_joueurs()[1].est_vide():
+            self.tour(self.get_liste_joueurs()[0], self.get_liste_joueurs()[1])
+            self.set_tour(self.get_tour() + 1)
+        print("Le gagnant est", self.gagnant())
 
     def __str__(self):
         """Affiche le jeu."""
@@ -46,3 +114,7 @@ class Jeu:
             s += str(joueur) + "\n"
         return s
 
+if __name__ == "__main__":
+    jeu = Jeu()
+    print(jeu)
+    jeu.partie()
